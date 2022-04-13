1dbb1b28f91bf6f319bc3effd8008faf76bcdec4
Eliot <eliotpitman@gmail.com>
2022-04-13 05:11:14 -0500

    adds index and show

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,32 @@
 <script>
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   data: function () {
-    return { places: [] };
+    return { places: {}, post: {} };
   },
   created: function () {
-    placesIndex:
-  }
+    axios.get("/places").then((response) => {
+      this.places = response.data;
+      console.log("all places", this.places);
+    });
+  },
+  methods: {
+    placesShow: function () {
+      axios.get("/places/" + this.$route.params.id).then((response) => {
+        console.log(response.data);
+        this.post = response.data;
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="place in places" :key="place.id">
+      <h1>{{ place.name }}</h1>
+      <h2>{{ place.address }}</h2>
+    </div>
   </div>
 </template>
